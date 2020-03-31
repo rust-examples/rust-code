12c82f632c3a1c110f66beeeec34ba941966c3bb
Greg Szabo <greg@philosobear.com>
2020-03-31 03:30:03 -0400

    Serde basic example

--- myapps/src/main.rs
+++ myapps/src/main.rs
@@ -2,9 +2,9 @@ extern crate myapps;
 
 use myapps::module1::public_submodule::public_function_with_too_common_or_weird_name as papperlapapp;
 #[allow(unused_imports)]
-use myapps::separatefile;
-#[allow(unused_imports)]
 use myapps::separatedir;
+#[allow(unused_imports)]
+use myapps::separatefile;
 #[allow(unused_imports)]
 use myapps::separatefileanddir;
 
---
+++ myserde/src/main.rs
@@ -0,0 +1,37 @@
+use serde_derive::{Deserialize, Serialize};
+use serde_json;
+
+// You'll need the `Deserialize` trait on your struct so serde can deserialize it.
+#[derive(Serialize, Deserialize)]
+struct MyStruct {
+    somestring: String,
+    someint: u64,
+}
+
+fn main() {
+    // Simple deserialize
+
+    // Note: use r#"string"# for raw strings with double-quotes
+    let incoming = r#"
+    {
+      "somestring": "Hello",
+      "someint": 4
+    }
+    "#;
+
+    // The type of the variable is explicitly given because serde doesn't know by itself what type to use.
+    let result: MyStruct = serde_json::from_str(incoming).unwrap();
+
+    // Use the resulting structure.
+    println!("{}", result.somestring);
+    println!("{}", result.someint);
+
+    // Simple serialize
+
+    let data = MyStruct {
+        somestring: "mystring".to_string(),
+        someint: 42,
+    };
+
+    println!("{}", serde_json::to_string(&data).unwrap());
+}
